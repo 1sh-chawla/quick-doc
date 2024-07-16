f1f4fe7b54fcc7f10e2bd8e6857ef19947dc794d
Vansh Chawla <vansh.chawla.ug21@nsut.ac.in>
2024-07-16 21:31:08 +0530

    Changes in server for deployment

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = 9000;
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: '*',
         methods: ['GET', 'POST']
     }
 });
